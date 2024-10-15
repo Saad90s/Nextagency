66781b62e1db213a3b452ec51756f17ff3e3cf35
unknown <rayanerayanecr7@gmail.com>
2024-10-15 21:14:43 +0100

    navbar home

--- ProjectAgency/src/App.jsx
+++ ProjectAgency/src/App.jsx
@@ -1,6 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
