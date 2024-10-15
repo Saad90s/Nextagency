b2b43b475784767cca5b21d5b0131f6b7cada16c
Saad <saadlarabi.05@gmail.com>
2024-10-15 21:21:33 +0100

    Services About

--- ProjectAgency/src/App.jsx
+++ ProjectAgency/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Services from "./components/Services";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Services/>
+      <About/>
     </>
   );
 }
