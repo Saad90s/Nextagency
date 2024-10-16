586cd279142089bb00499ed0171bb1e80ba8dc03
Saad <saadlarabi.05@gmail.com>
2024-10-16 18:03:38 +0100

    Newsletter MyFooter

--- ProjectAgency/src/App.jsx
+++ ProjectAgency/src/App.jsx
@@ -5,6 +5,8 @@ import Services from "./components/Services";
 import About from "./components/About";
 import Products from "./components/Products";
 import Blog from "./components/Blog";
+import Newsletter from "./components/Newsletter";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <About />
       <Products />
       <Blog />
+      <Newsletter/>
+      <MyFooter />
     </>
   );
 }
